3c1dcd16a9fbf21dcfb9ed47a404da8a3e2b0179
jorden <jorden.villmer@gmail.com>
2025-05-08 02:36:29 +0000

    Automatic RAM Allocation

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 
 echo "We out here building our root & rocking our kernel..."
+sudo systemctl start docker
 sudo docker build -t jdxbuildroot .
-sudo docker run -it jdxbuildroot
+TOTAL_RAM=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+ALLOCATED_RAM=$(( (TOTAL_RAM / 1024) - 4096 ))
+sudo docker run -it --memory="${ALLOCATED_RAM}m" jdxbuildroot
