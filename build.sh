8e81e0140054ef57cbc8404609ccc554ff06203e
jorden <jorden.villmer@gmail.com>
2025-05-08 02:52:31 +0000

    Default Allocation Subtraction to -6GB

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ sudo systemctl start docker
 sudo docker build -t jdxbuildroot .
 
 TOTAL_RAM=$(grep MemTotal /proc/meminfo | awk '{print $2}')
-DEFAULT_ALLOCATED_RAM=$(( (TOTAL_RAM / 1024) - 4096 ))
+DEFAULT_ALLOCATED_RAM=$(( (TOTAL_RAM / 1024) - 6144 ))
 
 echo "Default allocated RAM: ${DEFAULT_ALLOCATED_RAM} MB"
 read -p "Enter custom RAM allocation in GB (or press Enter to use default): " CUSTOM_RAM_GB
@@ -13,5 +13,5 @@ if [[ -z "$CUSTOM_RAM_GB" ]]; then
 else
     ALLOCATED_RAM=$(( CUSTOM_RAM_GB * 1024 ))
 fi
-
+echo "Allocated RAM: ${ALLOCATED_RAM} MB"
 sudo docker run -it --memory="${ALLOCATED_RAM}m" jdxbuildroot
