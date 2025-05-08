f89c941a9c20bd918850cf9946ba9ec8ed265126
jorden <jorden.villmer@gmail.com>
2025-05-08 03:44:54 +0000

    Formatting

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ sudo docker build -t jdxbuildroot .
 
 TOTAL_RAM=$(grep MemTotal /proc/meminfo | awk '{print $2}')
 DEFAULT_ALLOCATED_RAM=$(( (TOTAL_RAM / 1024) - 6144 ))
-
 echo
 echo "Default allocated RAM: ${DEFAULT_ALLOCATED_RAM} MB"
 read -p "Enter custom RAM allocation in GB (or press Enter to use default): " CUSTOM_RAM_GB
