1291703692a108aafa35dd7f5b2e5e4d1f677141
jorden074 <37036511+jorden074@users.noreply.github.com>
2025-05-14 07:47:48 -0400

    updated file paths in upload.sh

--- flashing/upload.sh
+++ flashing/upload.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-rkdeveloptool db rk3399_loader_v1.24.126.bin
-rkdeveloptool gpt parameter.txt
-rkdeveloptool wl 0x40 idbloader.img
-rkdeveloptool wl 0x4000 u-boot.itb
-rkdeveloptool wl 0x8000 boot.img
-rkdeveloptool wl 0x40000 rootfs.img
-rkdeveloptool rd
+rkdeveloptool db rk3399_loader_v1.24.126.bin #loader
+rkdeveloptool gpt parameter.txt #gpt
+rkdeveloptool wl 0x40 images/idbloader.img #idbloader
+rkdeveloptool wl 0x4000 images/u-boot.itb #uboot
+rkdeveloptool wl 0x8000 images/Image #kernel
+rkdeveloptool wl 0x40000 images/rootfs.ext2 #rootfs
+rkdeveloptool rd 
